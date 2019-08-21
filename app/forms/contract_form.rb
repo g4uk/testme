e1c9a44b0848a8a033c1033349414f837c1d4fed
Roman Hnatiuk <roman.hnatiuk@teamvoy.com>
2019-08-22 01:08:47 +0300

    add custom validators. fixed rubocop

--- app/forms/contract_form.rb
+++ app/forms/contract_form.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class ContractForm
   include ActiveModel::Model
 
@@ -7,11 +9,11 @@ class ContractForm
   validates :status, inclusion: { in: Contract.statuses.keys }
 
   validates :name, presence: true
-  validates :name, uniqueness: true
+  validate :verify_unique_name
   validates :name, length: { maximum: 255 }
 
   validates :start_date, presence: true
-  validates :start_date, unless: -> { Date.parse(start_date).past? }
+  validate :verify_future_start_date
 
   validates :avg_monthly_price,
             numericality: { greater_than_or_equal_to: 0, allow_nil: true }
@@ -23,5 +25,17 @@ class ContractForm
                      name: name,
                      start_date: start_date,
                      avg_monthly_price: avg_monthly_price)
+  end
+
+  def verify_unique_name
+    return unless Contract.exists?(name: name)
+
+    errors.add :name, 'has already been taken'
+  end
+
+  def verify_future_start_date
+    return unless Date.parse(start_date).past?
+
+    errors.add :start_date, 'in the past'
   end
 end
